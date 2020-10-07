6ed955236b05db6109cc9977ffe4159864867d62
Your Name <2528749320@qq.com>
2020-10-07 18:22:10 +0800

    number

---
+++ Chapter2/Exection/2_9.py
@@ -0,0 +1,2 @@
+favorite_number = 6
+print(f"My favorite number is {favorite_number}")
---
+++ Chapter2/Section4_Number/operation.py
@@ -0,0 +1,29 @@
+print(3+2)
+print(3-2)
+print(3*2)
+print(3/2)
+
+print(3**2)#三的平方
+print(3**3)#三的立方
+
+print(2+3*4)
+
+print(0.1+0.2)
+print(3*0.1)
+
+print(4/2)
+print(9/3)
+
+print(1+2.0)
+
+universe_age = 14_000_000_000#可以为了方便阅读用下划线将数隔开
+print(universe_age)
+
+um = 0.00_01
+print(um)
+
+x, y, z = 1, 10, 100
+print(x, y, z)
+
+MAX_CONNECTiONS = 10 #常量用大写表示，其值在程序整个生命周期中不变
+print(MAX_CONNECTiONS)
