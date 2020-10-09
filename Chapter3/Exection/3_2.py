94cf5ace022c14750c245599d6703b2af233f400
Your Name <2528749320@qq.com>
2020-10-09 17:19:54 +0800

    Exection

---
+++ Chapter3/Exection/3_2.py
@@ -0,0 +1,9 @@
+name = ['A', 'B', 'C', 'D']
+message = f"Hello! {name[0]}."
+print(message)
+message = f"Hello! {name[1]}."
+print(message)
+message = f"Hello! {name[2]}."
+print(message)
+message = f"Hello! {name[3]}."
+print(message)
---
+++ Chapter3/Section1_List/bicycles.py
@@ -0,0 +1,11 @@
+bicycles = ['trek', 'cannondale', 'redline','specialized']
+print(bicycles)
+
+print(bicycles[0])
+
+print(bicycles[0].title())
+
+print(bicycles[-1])#返回最后一个函数
+
+message = f"My first bicycles was a {bicycles[-1].title()}."
+print(message)
