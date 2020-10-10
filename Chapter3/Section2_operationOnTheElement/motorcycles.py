d10f802cdbc9581806042409a3177505241d9cbe
Your Name <2528749320@qq.com>
2020-10-10 12:43:23 +0800

    element

--- Chapter3/Section2_operationOnTheElement/motorcycles.py
+++ Chapter3/Section2_operationOnTheElement/motorcycles.py
@@ -17,13 +17,32 @@ print(letters)
 
 motorcycles.insert(0,'motor')
 print(motorcycles)
-
+#1
 del motorcycles[0]
 print(motorcycles)
 
 del motorcycles[1]
 print(motorcycles)
-
-popped_motorcycle = motorcycles.pop()
+#2
+popped_motorcycle = motorcycles.pop()#弹出元素
 print(motorcycles)
 print(popped_motorcycle)
+#3
+popped_motorcycle = motorcycles.pop(1)
+print(motorcycles)
+print(f"The second motorcycles I owned was a {popped_motorcycle.title()}.")
+#4
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+
+motorcycles.remove('yamaha')
+print(motorcycles)
+
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print(f"\nA {too_expensive.title()} is too expensive for me")
+
+letters = ['A', 'B', 'A']
+letters.remove('A')
+print(letters)
