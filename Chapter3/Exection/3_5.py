26bbda80bf6e4c590955c512e2094b88e2a7d51e
Your Name <2528749320@qq.com>
2020-10-10 13:31:27 +0800

    Exection

---
+++ Chapter3/Exection/3_5.py
@@ -0,0 +1,6 @@
+name = ['Albert Einstein', 'Bill Gates', 'Da Vinci']
+print(f"I'm going to invite {name[0]}, {name[1]} and {name[2]} to dinner")
+leave = name.pop()
+print(f"Just learned that {leave} couldn't come for some reason")
+name.append('My girl')
+print(f"I'm going to invite {name[0]}, {name[1]} and {name[2]} to dinner")
