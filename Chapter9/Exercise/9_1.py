fdd213d82cb3dcbbed0171f57ebf69a06981d251
Your Name <2528749320@qq.com>
2020-10-29 18:52:41 +0800

    Chapter9

---
+++ Chapter9/Exercise/9_1.py
@@ -0,0 +1,9 @@
+class restaurant:
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant = =restaurant_name
+        self.cuisine_type = cuisine_type
+        
+    def describe_restaurant(self):
+        print(f"My restaurant name is {restaurant_name}.")
+        print(f"And We have the following dishes:")
+        print(f"-{self.restaurant} -{self.cuisine_type}")
